43a93eb5984a99ca68cd77215f5bfac9c4168e3c
aneupane2940 <achyutneupane2940@gmail.com>
2017-08-18 16:59:26 -0700

    Update LotteryNumbers.java

--- src/LotteryNumbers.java
+++ src/LotteryNumbers.java
@@ -1,8 +1,7 @@
 import java.util.HashSet;
 import java.util.Set;
 
-public class LotteryNumbers {
-
+public class ToggleMain {
 	public static void main(String[] args) {
 		String s1 = "4938";
 		Set<Set<String>> all = new HashSet<>();
@@ -18,20 +17,26 @@ public class LotteryNumbers {
 		}
 		Set<Set<String>> startFirst = new HashSet<>();
 		startFirst.addAll(all);
+
 		if (all.isEmpty()) {
 			Set<String> start = new HashSet<>();
-			start.add("");
-			startFirst.add(start);
+			start.add(allNumbers.substring(0, 1));
+			all.add(start);
 		}
-		for (Set<String> s : startFirst) {
-			s.add(allNumbers.substring(0, 1));
+		for (Set<String> s : all) {
+			Set<String> small = new HashSet<>();
+			small.addAll(s);
+			small.add(allNumbers.substring(0, 1));
+			startFirst.add(small);
 		}
 		getlotteryNumbers(allNumbers.substring(1, allNumbers.length()), startFirst);
+		Set<Set<String>> secondSet = new HashSet<>();
 		if (allNumbers.length() > 1) {
-			Set<Set<String>> secondSet = new HashSet<>();
-			secondSet.addAll(all);
-			for (Set<String> s : secondSet) {
-				s.add(allNumbers.substring(0, 2));
+			for (Set<String> s : all) {
+				Set<String> small = new HashSet<>();
+				small.addAll(s);
+				small.add(allNumbers.substring(0, 2));
+				secondSet.add(small);
 			}
 			getlotteryNumbers(allNumbers.substring(2, allNumbers.length()), secondSet);
 		}
